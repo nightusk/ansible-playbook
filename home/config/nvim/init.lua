fbc890890fe046fcb3e9c6752567664f7222b14f
nightusk <nightusk@gmail.com>
2023-01-02 15:04:49 +0900

    neovim init.vim options

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -1,3 +1,63 @@
 -- vim: set foldmethod=marker:
 
 require('plugins')
+
+-- 1 important {{{
+-- }}}
+-- 2 moving around, searching and patterns {{{
+vim.opt.cdhome = true
+vim.opt.wrapscan = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- }}}
+-- 3 tags {{{
+-- }}}
+-- 4 displaying text {{{
+vim.opt.scrolloff = 5
+vim.opt.wrap = false
+vim.opt.list = true
+vim.opt.cursorline = true
+-- }}}
+-- 5 syntax, highlighting and spelling {{{
+-- }}}
+-- 6 multiple windows {{{
+-- }}}
+-- 7 multiple tab pages {{{
+-- }}}
+-- 8 terminal {{{
+-- }}}
+-- 9 using the mouse {{{
+-- }}}
+-- 10 printing {{{
+-- }}}
+-- 11 messages and info {{{
+-- }}}
+-- 12 selecting text {{{
+-- }}}
+-- 13 editing text {{{
+vim.opt.showmatch = true
+-- }}}
+-- 14 tabs and indenting {{{
+-- }}}
+-- 15 folding {{{
+-- }}}
+-- 16 diff mode {{{
+-- }}}
+-- 17 mapping {{{
+-- }}}
+-- 18 reading and writing files {{{
+-- }}}
+-- 19 the swap file {{{
+-- }}}
+-- 20 command line editing {{{
+-- }}}
+-- 21 executing external commands {{{
+-- }}}
+-- 22 running make and jumping to errors (quickfix) {{{
+-- }}}
+-- 23 language specific {{{
+-- }}}
+-- 24 multi-byte characters {{{
+-- }}}
+-- 25 various {{{
+-- }}}
